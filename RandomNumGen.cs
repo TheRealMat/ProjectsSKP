1a5a5ac2b5bab39aa42873f97619c2a8d010f34b
Mat <mat555333@gmail.com>
2020-03-16 10:26:09 +0100

    random number generator

---
+++ RandomNumGen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberGame
+{
+    class RandomNumGen
+    {
+
+        Random random = new Random();
+        public int RandomNumber(int min, int max)
+        {
+            return random.Next(min, max);
+        }
+
+    }
+}
