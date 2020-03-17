87b001502b37dd56cb8d196966e7f4e5c71e0dd3
Mat <mat555333@gmail.com>
2020-03-17 09:19:55 +0100

    polygon creation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -38,6 +38,17 @@ namespace CalculatorWPF
             {
 
             }
+
+            Polygon p = new Polygon();
+            p.Stroke = Brushes.Black;
+            p.Fill = Brushes.LightBlue;
+            p.StrokeThickness = 1;
+            p.HorizontalAlignment = HorizontalAlignment.Center;
+            p.VerticalAlignment = VerticalAlignment.Center;
+            p.Points = new PointCollection() { new Point(10, 0), new Point(-10, 0), new Point(-10, 10) };
+
+            Canvas1.Children.Add(p);
+
         }
 
         // how am i gonna draw a cone on a 2d plane hmm
