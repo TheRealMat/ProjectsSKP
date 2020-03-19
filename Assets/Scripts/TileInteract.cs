ba9d81e14391f5ac197d5c24021c0bd38a181513
Mat <mat555333@gmail.com>
2020-03-19 12:55:14 +0100

    get tile from mouseclick

---
+++ Assets/Scripts/TileInteract.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileInteract : MonoBehaviour
+{
+    public Grid grid; //Set a Grid or Tilemap object to this in the Editor
+    public Tilemap tileMap; //Set a Tilemap object to this in the Editor
+    public void Update()
+    {
+
+        //Detect when mouse is clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Get position of the mouseclick
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Convert position of the mouseclick to the position of the tile located at the mouseclick
+            Vector3Int coordinate = grid.WorldToCell(mouseWorldPos);
+
+            //Display tile position in log
+            Debug.Log(coordinate);
+            //Display the sprite value of the tile in log *SUCCESS*
+            Debug.Log(tileMap.GetSprite(coordinate));
+
+            // get reference to tile
+            tileMap.GetTile(coordinate);
+
+        }
+    }
+}
