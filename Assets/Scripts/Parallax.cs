0a7f7bf4a4b76336aad5f28b3d17e8e50255d9f8
Mat <mat555333@gmail.com>
2020-03-23 12:41:34 +0100

    added parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length;
+    private float startpos;
+    public GameObject cam;
+    public float parallaxAmount;
+
+    private void Start()
+    {
+        startpos = transform.position.x;
+        // length of sprite
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxAmount));
+        float dist = (cam.transform.position.x * parallaxAmount);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        // repeats background
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+
+    }
+}
