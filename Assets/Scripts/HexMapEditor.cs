f8ca9aaf21b1531cdfa04f1533f619c08d57bc9b
Mat <mat555333@gmail.com>
2020-03-27 09:58:50 +0100

    prevents map being edited underneath the ui

--- Assets/Scripts/HexMapEditor.cs
+++ Assets/Scripts/HexMapEditor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class HexMapEditor : MonoBehaviour
 {
@@ -18,7 +19,7 @@ public class HexMapEditor : MonoBehaviour
 
 	void Update()
 	{
-		if (Input.GetMouseButton(0))
+		if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
 		{
 			HandleInput();
 		}
