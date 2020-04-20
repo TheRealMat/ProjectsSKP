65fef151de34c483daab7eaf6c0ce09b9bd0d680
Mat <mat555333@gmail.com>
2020-04-20 10:19:12 +0200

    player jumping
    character controller already has an isGrounded check but it doesn't work

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform groundCheck;
 
     public float speed = 12f;
     public float gravity = -9.81f;
     public float groundDistance = 0.4f;
+    public float jumpHeight = 3f;
+    public LayerMask groundMask;
+    bool isGrounded;
 
     Vector3 velocity;
 
@@ -22,9 +26,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (controller.isGrounded)
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
         {
-            velocity.y = 0f;
+            velocity.y = -2f;
         }
 
         float x = Input.GetAxis("Horizontal");
@@ -33,6 +38,13 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+
 
         velocity.y += gravity * Time.deltaTime;
 
