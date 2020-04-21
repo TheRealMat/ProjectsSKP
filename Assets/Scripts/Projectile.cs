b3084b0249a73c47a86b327fb707188ca2adcb77
Mat <mat555333@gmail.com>
2020-04-21 10:58:47 +0200

    new projectiles

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -9,7 +9,10 @@ public class Projectile : MonoBehaviour
 
     private float counter;
     Rigidbody rigidBody;
-
+    public ParticleSystem pSystem;
+    public LayerMask groundMask;
+    public float groundDistance = 0.4f;
+    bool isGrounded;
     private void Start()
     {
         rigidBody = this.transform.GetComponent<Rigidbody>();
@@ -32,6 +35,15 @@ public class Projectile : MonoBehaviour
 
         if (counter >= killTime)
         {
+            Destroy(this.gameObject);
+        }
+
+        isGrounded = Physics.CheckSphere(transform.position, groundDistance, groundMask);
+        if (isGrounded)
+        {
+            pSystem.Stop();
+            pSystem.transform.parent = null;
+            Destroy(pSystem.gameObject, 5.0f); // if particles live for at most 5 secs
             Destroy(this.gameObject);
         }
     }
