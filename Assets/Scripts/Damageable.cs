e74944b4ff8963742dd844b8d4c930245520fcb0
Mat <mat555333@gmail.com>
2020-04-24 11:12:27 +0200

    things with health killable

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -6,8 +6,13 @@ public class Damageable : MonoBehaviour
 {
     public int maxHealth;
     public int health;
+    public bool getsDestroyed;
     public void takeDamage(int dmgValue)
     {
         health -= dmgValue;
+        if (health <= 0 && getsDestroyed == true)
+        {
+            Destroy(gameObject);
+        }
     }
 }
