29dad895dad3e45d97959df12c900ba6fd2e2d0e
Mat <mat555333@gmail.com>
2020-04-24 10:25:01 +0200

    removed unused code

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -9,16 +9,13 @@ public class Projectile : MonoBehaviour
 
     private float counter;
     Rigidbody rigidBody;
-    SphereCollider thisCollider;
     public List<ParticleSystem> pSystems;
     public LayerMask groundMask;
     public float groundDistance = 0.4f;
-    bool isGrounded;
     public int damage;
     private void Start()
     {
         rigidBody = this.transform.GetComponent<Rigidbody>();
-        thisCollider = transform.GetComponent<SphereCollider>();
     }
 
 
@@ -56,6 +53,8 @@ public class Projectile : MonoBehaviour
             Destroy(p.gameObject, 5f); // if particles live for at most 5 secs. it doesn't seem possible to get the actual lifetime of particles
         }
         Destroy(this.gameObject);
+
+        // checks if whatever we hit can take damage
         if (other.GetComponent<Damageable>() != null)
         {
             // can i get this from the if statement so i don't have to look it up twice?
