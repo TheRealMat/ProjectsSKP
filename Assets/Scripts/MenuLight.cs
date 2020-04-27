765c8d0d0b404b284d630f28e1ef279a84979f8b
Mat <mat555333@gmail.com>
2020-04-27 14:57:47 +0200

    menu mockup

---
+++ Assets/Scripts/MenuLight.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLight : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 position =  Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 2f));
+        transform.position = position;
+    }
+}
