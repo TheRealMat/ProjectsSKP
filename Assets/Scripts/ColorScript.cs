01e0cedfdd80440392526e7e52cee7fb07939531
Mat <mat555333@gmail.com>
2020-05-05 11:28:06 +0200

    color

---
+++ Assets/Scripts/ColorScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorScript : MonoBehaviour
+{
+    Color caucasian1 = new Color(255,224,209);
+    Color caucasian2 = new Color(252, 204, 179);
+    Color caucasian3 = new Color(232, 181, 155);
+    Color latino = new Color(217, 174, 150);
+    Color mediterranean = new Color(199, 155, 139);
+    Color asian1 = new Color(255, 222, 179);
+    Color asian2 = new Color(227, 186, 132);
+    Color arab = new Color(196, 145, 94);
+    Color indian = new Color(184, 120, 64);
+    Color african1 = new Color(117, 69, 35);
+    Color african2 = new Color(71, 28, 24);
+    Color albino = new Color(255, 244, 230);
+    Color orange = new Color(255, 201, 5);
+}
