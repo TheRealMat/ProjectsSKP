92d4df79be078f98ae4c16407327818c40538228
Mat <mat555333@gmail.com>
2020-05-06 14:03:11 +0200

    sprite directions

---
+++ Assets/Scripts/ObjectRotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotator : MonoBehaviour
+{
+    //List<SpriteDirections> components = new List<SpriteDirections>()
+    //{
+
+    //};
+
+    //private void Start()
+    //{
+    //    components = GetComponentsInChildren<SpriteDirections>();
+    //}
+}
---
+++ Assets/Scripts/SpriteDirections.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SpriteDirections : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    public List<Sprite> spriteDir = new List<Sprite>() { 
+
+    };
+
+    private void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void setSprite(int index)
+    {
+        spriteRenderer.sprite = spriteDir[index];
+    }
+
+
+}
