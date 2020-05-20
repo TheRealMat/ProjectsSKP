aad7cf756fe3bd900fd9bce1281704a8446caae2
Mat <mat555333@gmail.com>
2020-05-20 09:46:40 +0200

    desert objects

---
+++ Assets/Scripts/DesertObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesertObject : MonoBehaviour
+{
+    private Movement movement;
+    public float destroyDistance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = FindObjectOfType<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z >= destroyDistance)
+        {
+            Destroy(gameObject);
+        }
+
+        // magic number seems to have to do with size of the scrolling texture? i'm not sure
+        // i'm not really even sure it's the right speed, but it looks like it
+        transform.position += transform.forward * movement.speed * 10 * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/DoodadSpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoodadSpawner : MonoBehaviour
+{
+    public float minRange;
+    public float maxRange;
+    public float spawnDistance;
+    [Range(0.0f, 1.0f)]
+    public float spawnChance;
+    public float spawnTimeSeconds;
+    private float elapsedSeconds;
+
+    public GameObject cactus;
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void Update()
+    {
+        if (gameManager.gameStarted == true)
+        {
+            elapsedSeconds += Time.deltaTime;
+            if (elapsedSeconds >= spawnTimeSeconds)
+            {
+                elapsedSeconds = 0;
+
+                // checks if we want to spawn something
+                if (Random.value <= spawnChance)
+                {
+                    // checks which side of the road to spawn it on
+                    if (Random.value <= 0.5)
+                    {
+                        SpawnObject(cactus, minRange, maxRange, spawnDistance);
+                    }
+                    else
+                    {
+                        SpawnObject(cactus, -minRange, -maxRange, spawnDistance);
+                    }
+
+                }
+            }
+        }
+
+    }
+
+
+    public void SpawnObject(GameObject prefab, float min, float max, float distance)
+    {
+        Vector3 position = new Vector3(Random.Range(min, max), 0, distance);
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
