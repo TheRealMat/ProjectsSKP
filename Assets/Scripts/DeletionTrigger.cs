7128832f741b758a11c746f678dd2ffc2b910cb4
Mat <mat555333@gmail.com>
2020-05-27 09:54:48 +0200

    desert trash cleanup

---
+++ Assets/Scripts/DeletionTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeletionTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Doodad")) {
+            Destroy(other.gameObject);
+        }
+    }
+}
--- Assets/Scripts/DesertObject.cs
+++ Assets/Scripts/DesertObject.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class DesertObject : MonoBehaviour
 {
     private Movement movement;
-    public float destroyDistance;
 
 
     // Start is called before the first frame update
@@ -17,11 +16,6 @@ public class DesertObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z >= destroyDistance)
-        {
-            Destroy(gameObject);
-        }
-
         // magic number seems to have to do with size of the scrolling texture? i'm not sure
         // i'm not really even sure it's the right speed, but it looks like it
         transform.position += transform.forward * movement.speed * 10 * Time.deltaTime;
